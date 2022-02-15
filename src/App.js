127fb50d3652cd5b0bb51bad5cf1213a38acb30e
GUNA SGS <96234417+gunasgs@users.noreply.github.com>
2022-02-15 15:00:22 +0530

    Day 26

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './card.css';
+import Card from'./card';
+
+function App() {
+  return (
+    <div>
+   <Card></Card>
+    </div>
+  );
+}
+
+export default App;
